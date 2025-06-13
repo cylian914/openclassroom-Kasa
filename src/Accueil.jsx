ba0891575d7a15b449e6f1f85641aead5f10e2b0
cylian charbonnier <cylian.charbonnier1@gmail.com>
2025-06-13 03:09:25 +0200

    add Accueil

--- src/Accueil.jsx
+++ src/Accueil.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Link } from 'react-router'
 import './Accueil.scss'
 import onLoadedData from './data'
+import OverImage from './OverImage';
 
 
 
@@ -13,13 +14,15 @@ function Accueil() {
 
   return (
     <>
-      <h2 className='HeaderImageCaption'>Chez vous, partout et ailleurs</h2>
+    <OverImage height="20vh" img={"src/assets/AccueilBgImg.png"}>
+      <h2 className='OverImage-Mid acc-OverImage'>Chez vous, partout et ailleurs</h2>
+    </OverImage>
       <div className="grid">
         {data.map(d => {
           return (
             <Link to={"logement/" + d.id} className='gridImg' key={d.id}>
               <img src={d.cover} alt={d.description} />
-              <h3>{d.title}</h3>
+              <div><h3>{d.title}</h3></div>
             </Link>)
         })}
       </div >
